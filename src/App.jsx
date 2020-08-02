6f35ecae3402603dc2e48a1a4c89e2ede9be6371
AlexKarmaz <karmazct@mail.ru>
2020-08-03 00:50:55 +0300

    tests configured

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ class App extends React.Component{
   render(){
     return(
       <div className="App">
-        <h1> Helloooo, World!!! </h1>
+        <h1> Hello, World!!! </h1>
       </div>
     );
   }
