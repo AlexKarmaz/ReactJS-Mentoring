6eaf9fc2f312d9d645d72d41c7471a447171b29b
AlexKarmaz <karmazct@mail.ru>
2020-08-03 18:57:46 +0300

    webpack dev and prod config added

---
+++ webpack/webpack.config.common.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "../src/index.jsx",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/env"]
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader"]
+      },
+    ],
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "[name].bundle.js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
+};
---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.config.common");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+var CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  devServer: {
+    contentBase: path.join(__dirname, "dev"),
+    port: 3000,
+    publicPath: "http://localhost:3000/dist/",
+    hotOnly: true,
+  },
+  devtool: "inline-source-map",
+  optimization: {
+    minimize: false,
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin(),
+    new CaseSensitivePathsPlugin(),
+  ],
+});
